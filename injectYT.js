f1817368861e6975d3976d8546b7b2b48e22f204
Yuan Yu <tearsgundam@gmail.com>
2018-11-29 05:53:48 +0800

    add color gradiant

--- injectYT.js
+++ injectYT.js
@@ -1,9 +1,13 @@
 var queryURLTemplate = "https://www.googleapis.com/youtube/v3/commentThreads?pageToken={PAGETOKEN}&part=snippet%2Creplies&maxResults=50&order=relevance&videoId={VIDEOID}&fields=items(replies(comments(snippet(authorChannelUrl%2CauthorDisplayName%2CauthorProfileImageUrl%2ClikeCount%2CtextDisplay%2CtextOriginal%2CviewerRating)))%2Csnippet(topLevelComment(snippet(authorChannelUrl%2CauthorDisplayName%2CauthorProfileImageUrl%2ClikeCount%2CtextDisplay%2CtextOriginal))%2CtotalReplyCount))%2CnextPageToken&key={YOUR_API_KEY}"
 var YOURAPIKEY = "";
+var maxColor = [68,142,246];
+var minColor = [218,232,253];
 
 var player = document.getElementById('movie_player');
 var re = /((?<=v\=)(.*)(?=[&]))|((?<=v\=)(.*))/g;
 var lastURL = "";
+var currentLikeCounts = [];
+
 window.addEventListener('popstate',removeYTHL);
 
 
@@ -15,7 +19,7 @@ function formatQueryURL(key,videoID,pagetoken){
     return queryURL;
 }
   
-function nestDo(key,videoID,times,maxTimes,callback,pagetoken){
+function nestDo(key,videoID,times,maxTimes,callback,pagetoken,finalCallbock){
   if(typeof(pagetoken)== "undefined") pagetoken = "";
   let queryURL = formatQueryURL(key,videoID,pagetoken);
   axios.get(queryURL).then((response)=>{
@@ -24,8 +28,10 @@ function nestDo(key,videoID,times,maxTimes,callback,pagetoken){
     times += 1;
     if(times < maxTimes){
       if(typeof(data['nextPageToken']) != "undefined"){
-        nestDo(key,videoID,times,maxTimes,callback,data['nextPageToken'])
+        nestDo(key,videoID,times,maxTimes,callback,data['nextPageToken'],finalCallbock)
       }
+    }else{
+      finalCallbock();
     }
   })
 }
@@ -126,6 +132,7 @@ function createHighLight(timePoint,comment,duration){
   var point = document.createElement("YTHL");
   var position = Math.round((timePoint/duration) *100);
   point.title = comment['textOriginal'];
+  point.likeCount = comment['likeCount'];
   point.style.left = 'calc('+position+"% "+"- 3px)";
   point.onclick = function(event){
     event.stopPropagation();
@@ -133,9 +140,41 @@ function createHighLight(timePoint,comment,duration){
   };
   point.onmousedown = function(event){event.stopPropagation();};
   point.onmouseup = function(event){event.stopPropagation();};
+  currentLikeCounts.push(point.likeCount);
   return point;
 }
-
+function getLinkCountRange(){
+  currentLikeCounts.sort();
+  var range = {};
+  var q1 = currentLikeCounts[Math.floor((currentLikeCounts.length * 0.25))];
+  var q3  = currentLikeCounts[Math.ceil((currentLikeCounts.length * 0.75))];
+  var iqr = q3-q1;
+  range.max = q3 + iqr*1.5;
+  range.min = q1 - iqr*1.5;
+  range.min = (range.min>0)?range.min:0;
+  return range;
+}
+function colorPointByLinkCount(point,max,min){
+  if( (point.likeCount > max) || max == min){
+    point.style.backgroundColor = "rgb("+maxColor[0]+","+maxColor[1]+","+maxColor[2]+")";
+  }else if(point.likeCount < min){
+    point.style.backgroundColor = "rgb("+minColor[0]+","+minColor[1]+","+minColor[2]+")";
+  }else{
+    var tintFactor = 1 - ((point.likeCount-min) / (max-min));
+    var r = maxColor[0] + ((minColor[0] - maxColor[0]) * tintFactor);
+    var g = maxColor[1] + ((minColor[1] - maxColor[1]) * tintFactor);
+    var b = maxColor[2] + ((minColor[2] - maxColor[2]) * tintFactor);
+    point.style.backgroundColor = "rgb("+r+","+g+","+b+")";
+  }
+}
+function colorPoints(){
+  var YTHLs = document.getElementsByTagName('YTHL');
+  var range = getLinkCountRange();
+  for(var i=0;i<YTHLs.length;i++){
+    var YTHL = YTHLs[i];
+    colorPointByLinkCount(YTHL,range.max,range.min);
+  }
+}
 function removeYTHL(){
   var YTHLs = document.getElementsByTagName('YTHL');
   var length = YTHLs.length;
@@ -148,8 +187,9 @@ function removeYTHL(){
 function runYTHL(){
   if(document.URL != lastURL){
     lastURL = document.URL;
+    currentLikeCounts = [];
     var videoID = re.exec(document.URL)[0];
     while(re.exec(document.URL)){};
-    nestDo(YOURAPIKEY,videoID,0,5,renderBar,"");
+    nestDo(YOURAPIKEY,videoID,0,5,renderBar,"",colorPoints);
   }
 }
