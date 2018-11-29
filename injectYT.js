cb3aeac1c9768bdab14d03b0a5162dfedad7af47
Yuan Yu <tearsgundam@gmail.com>
2018-11-30 04:57:35 +0800

    add popup

--- injectYT.js
+++ injectYT.js
@@ -2,7 +2,7 @@ var queryURLTemplate = "https://www.googleapis.com/youtube/v3/commentThreads?pag
 var YOURAPIKEY = "";
 var maxColor = [68,142,246];
 var minColor = [218,232,253];
-
+var tooltipSize = 360;
 var player = document.getElementById('movie_player');
 var re = /((?<=v\=)(.*)(?=[&]))|((?<=v\=)(.*))/g;
 var lastURL = "";
@@ -29,6 +29,8 @@ function nestDo(key,videoID,times,maxTimes,callback,pagetoken,finalCallbock){
     if(times < maxTimes){
       if(typeof(data['nextPageToken']) != "undefined"){
         nestDo(key,videoID,times,maxTimes,callback,data['nextPageToken'],finalCallbock)
+      }else{
+        finalCallbock();
       }
     }else{
       finalCallbock();
@@ -103,6 +105,7 @@ function textTime2Sencond(textTime){
   }
   return unitInSecond;
 }
+
 function renderBar(data){
   var comments = handleRawout(data);
   var duration = player.getDuration();
@@ -129,47 +132,63 @@ function createHighLights(comment,duration){
 }
 
 function createHighLight(timePoint,comment,duration){
-  var point = document.createElement("YTHL");
+  var highLighElement = document.createElement("YTHL");
   var position = Math.round((timePoint/duration) *100);
-  point.title = comment['textOriginal'];
-  point.likeCount = comment['likeCount'];
-  point.style.left = 'calc('+position+"% "+"- 3px)";
-  point.onclick = function(event){
+  highLighElement.likeCount = comment['likeCount'];
+  highLighElement.style.left = 'calc('+position+"% "+"- 3px)";
+  highLighElement.onclick = function(event){
     event.stopPropagation();
     player.seekTo(timePoint,true);
   };
-  point.onmousedown = function(event){event.stopPropagation();};
-  point.onmouseup = function(event){event.stopPropagation();};
-  currentLikeCounts.push(point.likeCount);
-  return point;
+  highLighElement.onmousedown = function(event){event.stopPropagation();};
+  highLighElement.onmouseup = function(event){event.stopPropagation();};
+  highLighElement.onmouseover = function(event){ changeTooltipPositon(this);};
+  currentLikeCounts.push(highLighElement.likeCount);
+  highLighElement.innerHTML = '<div class="tooltip">'+comment['textDisplay'] + '</div>';
+  var tooltip = highLighElement.querySelector(".tooltip");
+  tooltip.onmousemove = function(event){event.stopPropagation();};
+  tooltip.onclick = function(event){event.stopPropagation();};
+  return highLighElement;
+}
+function changeTooltipPositon(highLighElement){
+  var currentTooltipLeft = highLighElement.offsetLeft - tooltipSize*0.5;
+  var currentTooltipRight = highLighElement.offsetLeft + tooltipSize*0.5;
+  var parentWidth = highLighElement.offsetParent.offsetWidth;
+  var tooltip = highLighElement.querySelector(".tooltip");
+  if(currentTooltipLeft<0){
+    tooltip.style.marginLeft = -highLighElement.offsetLeft + "px";
+  }else if(currentTooltipRight>parentWidth){
+    tooltip.style.marginLeft = -(currentTooltipRight - parentWidth + tooltipSize*0.5) + "px";
+  }
 }
 function getLinkCountRange(){
   currentLikeCounts.sort((a,b)=>{return a-b});
   var range = {};
-  var q1 = currentLikeCounts[Math.floor((currentLikeCounts.length * 0.25))];
-  var q3  = currentLikeCounts[Math.ceil((currentLikeCounts.length * 0.75))];
+  var q1 = currentLikeCounts[Math.ceil((currentLikeCounts.length * 0.25))];
+  var q3  = currentLikeCounts[Math.floor((currentLikeCounts.length * 0.75))];
   var iqr = q3-q1;
-  range.max = q3 + iqr*1.5;
-  range.min = q1 - iqr*1.5;
+  range.max = q3 + iqr*1.2;
+  range.min = q1 - iqr*1.2;
   range.min = (range.min>0)?range.min:0;
   return range;
 }
 
-function colorPointByLinkCount(point,max,min){
-  if( (point.likeCount > max) || max == min){
-    point.style.backgroundColor = "rgb("+maxColor[0]+","+maxColor[1]+","+maxColor[2]+")";
-  }else if(point.likeCount < min){
-    point.style.backgroundColor = "rgb("+minColor[0]+","+minColor[1]+","+minColor[2]+")";
+function colorPointByLinkCount(highLighElement,max,min){
+  if( (highLighElement.likeCount > max) || max == min){
+    highLighElement.style.zIndex = 61;
+    highLighElement.style.backgroundColor = "rgb("+maxColor[0]+","+maxColor[1]+","+maxColor[2]+")";
+  }else if(highLighElement.likeCount < min){
+    highLighElement.style.backgroundColor = "rgb("+minColor[0]+","+minColor[1]+","+minColor[2]+")";
   }else{
-    var tintFactor = 1 - ((point.likeCount-min) / (max-min));
+    var tintFactor = 1 - ((highLighElement.likeCount-min) / (max-min));
     var r = maxColor[0] + ((minColor[0] - maxColor[0]) * tintFactor);
     var g = maxColor[1] + ((minColor[1] - maxColor[1]) * tintFactor);
     var b = maxColor[2] + ((minColor[2] - maxColor[2]) * tintFactor);
-    point.style.backgroundColor = "rgb("+r+","+g+","+b+")";
+    highLighElement.style.backgroundColor = "rgb("+r+","+g+","+b+")";
   }
 }
 
-function colorPoints(){
+function colorhighLighElement(){
   var YTHLs = document.getElementsByTagName('YTHL');
   var range = getLinkCountRange();
   for(var i=0;i<YTHLs.length;i++){
@@ -181,6 +200,7 @@ function colorPoints(){
 function removeYTHL(){
   var YTHLs = document.getElementsByTagName('YTHL');
   var length = YTHLs.length;
+  lastURL = "";
   for(var i=0;i<length;i++){
     let YTHL = YTHLs[0];
     YTHL.parentNode.removeChild(YTHL);
@@ -193,6 +213,6 @@ function runYTHL(){
     currentLikeCounts = [];
     var videoID = re.exec(document.URL)[0];
     while(re.exec(document.URL)){};
-    nestDo(YOURAPIKEY,videoID,0,5,renderBar,"",colorPoints);
+    nestDo(YOURAPIKEY,videoID,0,5,renderBar,"",colorhighLighElement);
   }
 }
